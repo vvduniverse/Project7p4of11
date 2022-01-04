1b2e0b84de167b28eaac6aa60109754d6dd19cfc
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-04 10:17:26 +0200

    Initial Commit

---
+++ Project7p4of11/Project7p4of11App.swift
@@ -0,0 +1,17 @@
+//
+//  Project7p4of11App.swift
+//  Project7p4of11
+//
+//  Created by Vahtee Boo on 04.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Project7p4of11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
