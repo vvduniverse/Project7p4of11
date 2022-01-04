f2142c070bb5431352a9afdf95f4653a52de27b0
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-04 10:32:03 +0200

    Initial commit

--- Project7p4of11/ContentView.swift
+++ Project7p4of11/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+        VStack {
+            List {
+                ForEach(numbers, id: \.self) {
+                    Text("Row \($0)")
+                }
+                .onDelete(perform: removeRows)
+            }
+            
+            Button("Add Number") {
+                numbers.append(currentNumber)
+                currentNumber += 1
+            }
+        }
+        .toolbar {
+            EditButton()
+        }
+    }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
